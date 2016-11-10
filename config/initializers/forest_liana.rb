60484539000ac465d3fae2671dcfc3eb0b5621bd
JFachetti <goillot.jeremy@gmail.com>
2016-11-10 18:54:50 +0100

    Press Section"

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,2 +1,2 @@
-ForestLiana.secret_key = '60d21effd61cc35676e31067aa51107c0d88d8930e90bf3be420c8af4b33b2c1'
-ForestLiana.auth_key = 'c6RLZ0j-Eefidp1WMN1U8A'
+# ForestLiana.secret_key = '60d21effd61cc35676e31067aa51107c0d88d8930e90bf3be420c8af4b33b2c1'
+# ForestLiana.auth_key = 'c6RLZ0j-Eefidp1WMN1U8A'
